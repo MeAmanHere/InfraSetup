f962f046a628006f6302443e2c726ae7a932a367
Aman Mishra <Amanm186@Gmail.com>
2023-06-06 14:36:52 +0530

    Update main.tf

--- Infrastructure/terraform/env/dev/main.tf
+++ Infrastructure/terraform/env/dev/main.tf
@@ -31,3 +31,21 @@ variable "location" {
   type    = string
   default = "westeurope"
 }
+
+locals {
+  tags = {
+    environment     = var.env
+    created_by      = "Terraform"
+    application     = "K PIM Product API"
+    IT-Owner        = "sergei.ilin@kesko.fi"
+  }
+}
+
+module "resourcegroup" {
+  source                        = "../../resourcegroup"
+  env                           = var.env
+  environment                   = var.environment
+  loc                           = var.loc
+  location                      = var.location
+  tags                          = local.tags
+}
